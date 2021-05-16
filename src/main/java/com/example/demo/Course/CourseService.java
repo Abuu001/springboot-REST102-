2ccc8bf1ea1b55cfc40900df7778aabe369484bd
Abuu001 <abulugz@gmail.com>
2021-05-16 11:54:41 +0300

    configured findbytopicid method

--- src/main/java/com/example/demo/Course/CourseService.java
+++ src/main/java/com/example/demo/Course/CourseService.java
@@ -1,4 +1,4 @@
-package com.example.demo.Hello.Course;
+package com.example.demo.Course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,55 +9,37 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class TopicService{
+public class CourseService {
 
     @Autowired
-    private TopicRepository topicRepository;
+    private CourseRepository courseRepository;
 
-    private List<Course> topics = new ArrayList<>(Arrays.asList(
-            new Course("spring","Spring Framework","Spring Framework Description"),
-            new Course("Java","Java Framework","Java Framework Description"),
-            new Course("Javascript","Javascript Framework","Javascript Framework Description"),
-            new Course("Golang","Golang Framework","Golang Framework Description")
-    ));
+    public  List<Course> getAllCourses(String topicId){
 
-    public  List<Course> getAllTopics(){
-      //  return topics;
-        List<Course> topics =new ArrayList<>();
-         topicRepository.findAll()
-                .forEach(topics::add);
+        List<Course> courses =new ArrayList<>();
+        courseRepository.findByTopicId(topicId)
+                .forEach(courses::add);
 
-         return  topics;
+         return  courses;
     }
 
-    public Optional<Course> getTopic(String topicId){
-       // return topics.stream().filter(t -> t.getId().equals(topicId)).findFirst().get();
-        return topicRepository.findById(topicId);
+    public Optional<Course> getCourse(String topicId){
+
+        return courseRepository.findById(topicId);
     }
 
-    public void addTopic(Course newTopic){
-      //  topics.add(newTopic);
+    public void addCourse(Course course){
 
-        topicRepository.save(newTopic);
+        courseRepository.save(course);
     }
 
-    public void updateTopic(String topicId, Course newTopic) {
-    /*
-       for(int i=0;i < topics.size(); i++){
-           Topic t = topics.get(i);
+    public void updateCourse(Course newTopic) {
 
-           if(t.getId().equals(topicId)){
-               topics.set(i,newTopic);
-               return;
-           }
-       }
-    */
-
-      topicRepository.save(newTopic);
+        courseRepository.save(newTopic);
     }
 
-    public void deleteTopic(String topicId){
-       // topics.removeIf(t -> t.getId().equals(topicId));
-        topicRepository.deleteById(topicId);
+    public void deleteCourse(String topicId){
+
+        courseRepository.deleteById(topicId);
     }
 }
