f9d1a49ce548cf20cbdb01bd8643261fe2b4efc0
Abuu001 <abulugz@gmail.com>
2021-05-16 11:53:43 +0300

    added findbytopicid method

--- src/main/java/com/example/demo/Course/CourseRepository.java
+++ src/main/java/com/example/demo/Course/CourseRepository.java
@@ -1,4 +1,15 @@
 package com.example.demo.Course;
 
-public interface CourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course,String> {
+
+    public List<Course> findByTopicId(String topicId);
+
+    public List<Course> findByName(String name);
+
+    public List<Course> findByDescription(String description);
+
 }
