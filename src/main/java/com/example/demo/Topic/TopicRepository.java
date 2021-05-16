96e1e6f04b480f986b6924beadfd20ab4e442aac
Abuu001 <abulugz@gmail.com>
2021-05-16 11:55:34 +0300

    md

--- src/main/java/com/example/demo/Topic/TopicRepository.java
+++ src/main/java/com/example/demo/Topic/TopicRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.Topic;public interface TopicRepository {
+package com.example.demo.Topic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic,String> {
+
 }
