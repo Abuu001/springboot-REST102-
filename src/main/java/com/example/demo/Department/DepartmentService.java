49f1ace91bf6703a4f00cfddadec45efc9b13b9e
Abuu001 <abulugz@gmail.com>
2021-05-27 23:45:01 +0300

    department service

--- src/main/java/com/example/demo/Department/DepartmentService.java
+++ src/main/java/com/example/demo/Department/DepartmentService.java
@@ -1,4 +1,15 @@
 package com.example.demo.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
+   public  Department saveDepartment(Department newDepartment);
+
+   List<Department> getAllDepartments();
+
+   void removeDeparment(Long departmentId);
+
+   Department updateDepartment(Long departmentId, Department newDepartment);
+
+   Department getDepartmentByName(String departmentName);
 }
