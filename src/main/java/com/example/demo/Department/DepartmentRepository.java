acc86202edae6a2c4103bc2e6f9d159fa5ca4fad
Abuu001 <abulugz@gmail.com>
2021-05-27 23:44:39 +0300

    department repo

--- src/main/java/com/example/demo/Department/DepartmentRepository.java
+++ src/main/java/com/example/demo/Department/DepartmentRepository.java
@@ -1,2 +1,16 @@
-package com.example.demo.Department;public interface DepartmentRepository {
+package com.example.demo.Department;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+
+    Department findByDepartmentName(String departmentName);
+   /*
+   ...IgnoreCase used for ignoring the case sensitivity hence can match small and capital letter
+
+  @Query(value = "SELECT * FROM department WHERE departmentName = ?1", nativeQuery = true)
+    Department findByDepartmentNameIgnoreCase(String departmentName);
+   */
 }
