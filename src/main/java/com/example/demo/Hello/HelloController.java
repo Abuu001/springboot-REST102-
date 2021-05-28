c43d0bad8425f8256e73ef87db7753dc781e3f61
Abuu001 <abulugz@gmail.com>
2021-05-28 19:55:37 +0300

    Read value from properties file

--- src/main/java/com/example/demo/Hello/HelloController.java
+++ src/main/java/com/example/demo/Hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,14 @@ import java.util.List;
 
 @RestController
 public class HelloController {
+
+    @Value("${welcome.message}")  // can be used to read values from properties file
+    private String welcomeMessage;
+
+    @RequestMapping("/hello101")
+    public String Hello2(){
+        return welcomeMessage;
+    }
 
     @RequestMapping("/hello")
     public List Hello(){
