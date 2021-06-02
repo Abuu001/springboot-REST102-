14fa48b95382d83ced30b63b259c0d4415c3671b
Abuu001 <abulugz@gmail.com>
2021-06-02 23:56:40 +0300

    Product props

--- src/main/java/com/example/demo/Product/Product.java
+++ src/main/java/com/example/demo/Product/Product.java
@@ -1,2 +1,27 @@
-package com.example.demo.Product;public class Product {
+package com.example.demo.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Product {
+
+    @Id
+    private int pid;
+    private String productName;
+    private  int quantity;
+    private int price;
+
+    public Product(String productName, int quantity, int price) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
 }
