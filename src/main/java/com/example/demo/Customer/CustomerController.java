7c1f8d2ddf07fc971e247f78c3c73efdacda3487
Abuu001 <abulugz@gmail.com>
2021-06-02 23:58:52 +0300

    customer REST

--- src/main/java/com/example/demo/Customer/CustomerController.java
+++ src/main/java/com/example/demo/Customer/CustomerController.java
@@ -1,2 +1,34 @@
-package com.example.demo.Customer;public class CustomerController {
+package com.example.demo.Customer;
+
+ import com.example.demo.Product.ProductRepository;
+ import lombok.AllArgsConstructor;
+ import lombok.NoArgsConstructor;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.web.bind.annotation.*;
+
+ import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping(path = "/getInfo")
+    public List<OrderResponse> getJoinInformation(){
+        return customerService.getInfo();
+    }
+
+    @PostMapping("/placeorder")
+    public Customer placeOrder(@RequestBody Customer request){
+        return customerService.placeOrder(request);
+    }
+
+    @GetMapping("/findallorders")
+    public List<Customer> getAllOrders(){
+        return customerService.getAllOrders();
+    }
 }
