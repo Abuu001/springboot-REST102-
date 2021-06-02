11bf008e9b7fafa403fee16615844d39e73a8855
Abuu001 <abulugz@gmail.com>
2021-06-02 23:57:09 +0300

    jpa operations

--- src/main/java/com/example/demo/Customer/CustomerRepository.java
+++ src/main/java/com/example/demo/Customer/CustomerRepository.java
@@ -1,2 +1,14 @@
-package com.example.demo.Customer;public interface CustomerRepository {
+package com.example.demo.Customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer>{
+
+    @Query("SELECT new com.example.demo.Customer.OrderResponse(c.name,p.productName) FROM Customer c JOIN c.products p")
+     List<OrderResponse> getJoinInformation();
 }
--- src/main/java/com/example/demo/Product/ProductRepository.java
+++ src/main/java/com/example/demo/Product/ProductRepository.java
@@ -1,2 +1,8 @@
-package com.example.demo.Product;public interface ProductRepository {
+package com.example.demo.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Integer> {
 }
