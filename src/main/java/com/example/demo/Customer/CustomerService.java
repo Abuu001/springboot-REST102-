9fbadfa61b83b2bbe2e5372449aa14e4741fdc0b
Abuu001 <abulugz@gmail.com>
2021-06-02 23:58:30 +0300

    customer functionality

--- src/main/java/com/example/demo/Customer/CustomerService.java
+++ src/main/java/com/example/demo/Customer/CustomerService.java
@@ -1,2 +1,36 @@
-package com.example.demo.Customer;public class CustomerService {
+package com.example.demo.Customer;
+
+import com.example.demo.Product.ProductRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerService {
+
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Customer placeOrder(Customer request) {
+        return customerRepository.save(request);
+    }
+
+    public List<Customer> getAllOrders() {
+        return customerRepository.findAll();
+    }
+
+    public List<OrderResponse> getInfo() {
+        return  customerRepository.getJoinInformation();
+    }
+
 }
