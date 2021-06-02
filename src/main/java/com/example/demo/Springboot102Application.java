5406b3c6629df3ad105be59e09a2208873b3f375
Abuu001 <abulugz@gmail.com>
2021-06-02 22:06:42 +0300

    added commandlinerunner

--- src/main/java/com/example/demo/Springboot102Application.java
+++ src/main/java/com/example/demo/Springboot102Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.Teacher.Teacher;
+import com.example.demo.Teacher.TeacherRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Springboot102Application {
@@ -11,4 +15,17 @@ public class Springboot102Application {
 		SpringApplication.run(Springboot102Application.class, args);
 	} 
 
+	/*@Bean
+	CommandLineRunner commandLineRunne(TeacherRepository teacherRepo){
+		return args -> {
+			Teacher t1= new Teacher(
+					"Maria",
+					"Leah",
+					"maria@teachers.edu",
+					33
+			);
+
+			teacherRepo.save(t1);
+		};
+	}*/
 }
