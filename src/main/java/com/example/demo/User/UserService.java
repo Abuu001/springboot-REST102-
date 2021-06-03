4853d90bed1e9f3fceb3211c8ca90e30b4749a6b
Abuu001 <abulugz@gmail.com>
2021-06-03 23:09:38 +0300

    User service

--- src/main/java/com/example/demo/User/UserService.java
+++ src/main/java/com/example/demo/User/UserService.java
@@ -1,2 +1,19 @@
-package com.example.demo.User;public class UserService {
+package com.example.demo.User;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public class UserService {
+
+    private EntityManager entityManager;
+
+    public long insert(User user){
+        entityManager.persist(user);
+
+        return user.getId();
+    }
 }
