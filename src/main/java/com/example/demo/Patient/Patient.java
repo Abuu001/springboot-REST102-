c4cd8bc0cfeaffdf7c53404593389eeb5d7714d0
Abuu001 <abulugz@gmail.com>
2021-06-10 19:59:36 +0300

    patient props

--- src/main/java/com/example/demo/Patient/Patient.java
+++ src/main/java/com/example/demo/Patient/Patient.java
@@ -1,2 +1,14 @@
-package com.example.demo.Patient;public class Patient {
+package com.example.demo.Patient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class Patient {
+
+    private final Integer patientId;
+    private final String patientName;
+
 }
