b9420aaa5d58683568b19db91ea78ddc525f4c58
Abuu001 <abulugz@gmail.com>
2021-06-10 20:01:12 +0300

    added enum for users role

--- src/main/java/com/example/demo/User/UserRole.java
+++ src/main/java/com/example/demo/User/UserRole.java
@@ -1,2 +1,20 @@
-package com.example.demo.User;public enum UserRole {
+package com.example.demo.User;
+
+import com.google.common.collect.Sets;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+import static com.example.demo.User.UserPermission.*;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+    PATIENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,PATIENT_READ,PATIENT_WRITE)), //should read n write
+    ADMIN_TRAINEE(Sets.newHashSet(COURSE_READ,PATIENT_READ)); //should only read
+
+    private final Set<UserPermission> permission;
+
 }
