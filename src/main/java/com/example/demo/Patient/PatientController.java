00983c66fd3b8ed2ce1cd40a658273ef57bdb80d
Abuu001 <abulugz@gmail.com>
2021-06-11 23:39:59 +0300

    md

--- src/main/java/com/example/demo/Patient/PatientController.java
+++ src/main/java/com/example/demo/Patient/PatientController.java
@@ -1,2 +1,29 @@
-package com.example.demo.Patient;public class PatientController {
+package com.example.demo.Patient;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/patient")
+public class PatientController {
+
+    private static final List<Patient> PATIENTS = Arrays.asList(
+            new Patient(1,"Naomi"),
+            new Patient(2,"Abu"),
+            new Patient(3,"James")
+    );
+
+    @GetMapping(path = "{patientId}")
+    public Patient getPatient(@PathVariable("patientId") Integer patientId){
+        return PATIENTS.stream()
+                .filter(patient -> patientId.equals(patient.getPatientId()))
+                .findFirst()
+                .orElseThrow(()->new IllegalStateException("Patient " + patientId + " doesn't exist"));
+    }
+
 }
