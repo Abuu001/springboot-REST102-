476c1bd0e480ae651b64d63d5cf938fe019f721c
Abuu001 <abulugz@gmail.com>
2021-06-11 23:39:07 +0300

    permissions for users role

--- src/main/java/com/example/demo/User/UserPermission.java
+++ src/main/java/com/example/demo/User/UserPermission.java
@@ -1,2 +1,15 @@
-package com.example.demo.User;public enum UserPermission {
+package com.example.demo.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserPermission {
+    PATIENT_READ("patient:read"),
+    PATIENT_WRITE("patient:write"),
+    COURSE_READ("course:read"),
+    COURSE_WRITE("course:write");
+
+    private final String permission;
+
 }
