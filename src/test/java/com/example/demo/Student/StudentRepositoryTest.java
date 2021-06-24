c52122834f696dadf26e105b3fe5a351e3560ce0
Abuu001 <abulugz@gmail.com>
2021-06-24 15:32:22 +0300

    config for test mode

---
+++ src/test/java/com/example/demo/Student/StudentRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.example.demo.Student;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StudentRepositoryTest {
+
+    @Test
+    void findStudentByEmail() {
+    }
+}
---
+++ src/test/java/com/example/demo/Student/StudentServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.demo.Student;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StudentServiceTest {
+
+    @Test
+    void getStudents() {
+    }
+
+    @Test
+    void addNewStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+
+    @Test
+    void updateStudent() {
+    }
+}
